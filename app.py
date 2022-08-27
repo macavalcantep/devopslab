75a8e6ac12717dc7bde4d2e2123a485242a57625
macavalcantep <33917737+macavalcantep@users.noreply.github.com>
2022-08-27 15:02:42 -0300

    App Final

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - v2 Docker"
+    return "Hello World - ACABOU"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
